1e531c774bbd32ee261b87bc4c3358b6a9b100c1
chirudevops3 <67657986+chirudevops3@users.noreply.github.com>
2024-06-16 14:15:29 +0000

    adding new line

--- EC2_Instance/main.tf
+++ EC2_Instance/main.tf
@@ -3,6 +3,26 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-0f58b397bc5c1f2e8"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    tags = {
+        Name = "newterra"
+    }
+}
+resource "aws_eip" "eip" {
+  #  instance = aws_instance.example.id
+    domain = "vpc"
+}
+resource "aws_eip_association" "ip" {
+  instance_id = aws_instance.example.id
+  allocation_id = aws_eip.eip.id
+}
+output "ec2_publicip" {
+    value = aws_instance.example.public_ip
+}
+output "ec2_id" {
+    value = aws_instance.example.id
+}
+output "eip_id" {
+    value = aws_eip_association.ip.id
 }
