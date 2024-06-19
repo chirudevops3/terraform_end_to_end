211b5c6b91267a57e02f0a782b8f6d5f357d04a5
chirudevops3 <67657986+chirudevops3@users.noreply.github.com>
2024-06-19 19:38:35 +0000

    Adding Variables

---
+++ variables_example/security_group.tf
@@ -0,0 +1,39 @@
+
+provider "aws" {
+  # Configuration options
+  region = "ap-south-1" # Set your desired AWS region
+}
+
+resource "aws_security_group" "allow_tls" {
+  name = "allow_tls"
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+  ingress {
+    description = "http port"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+  ingress {
+    description = "jenkins port"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow_tls"
+  }
+}
